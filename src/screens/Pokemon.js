248dab9118a8247e90c5cc26e6b464d59c309ff6
Jose Alberto Zavala Flores <jose.alberto.zavala.flores@gmail.com>
2021-11-06 23:30:48 -0700

    Se agregaron los stats

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,34 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import { ScrollView } from 'react-native';
-import { getPokemonDetailsApi } from '../api/pokemon';
-import { Header } from '../components/Pokemon/Header';
-import { Type } from '../components/Pokemon/Type';
+import React, { useState, useEffect } from "react";
+import { ScrollView } from "react-native";
+import { getPokemonDetailsApi } from "../api/pokemon";
+import { Header } from "../components/Pokemon/Header";
+import { Type } from "../components/Pokemon/Type";
+import { Stats } from "../components/Pokemon/Stats";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
-export const Pokemon = ({navigation, route: {params}}) => {
+export const Pokemon = ({ navigation, route: { params } }) => {
   const [pokemon, setPokemon] = useState(null);
 
   useEffect(() => {
-    (async() => {
-      try{
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          color="#fff"
+          size={20}
+          style={{ marginLeft: 20 }}
+          onPress={ navigation.goBack}
+        />
+      ),
+    });
+  }, [navigation, params]);
+
+  useEffect(() => {
+    (async () => {
+      try {
         const response = await getPokemonDetailsApi(params.id);
-        setPokemon(response)
-      }catch(error){
+        setPokemon(response);
+      } catch (error) {
         navigation.goBack();
       }
-    })()
-  },[params]);
+    })();
+  }, [params]);
 
-  if(!pokemon) return null;
+  if (!pokemon) return null;
 
   return (
     <ScrollView>
-      <Header 
-        name={pokemon.name} 
+      <Header
+        name={pokemon.name}
         order={pokemon.order}
         image={pokemon.sprites.other["official-artwork"].front_default}
         type={pokemon.types[0].type.name}
       />
       <Type types={pokemon.types} />
+      <Stats stats={pokemon.stats} />
     </ScrollView>
-  )
-}
+  );
+};
