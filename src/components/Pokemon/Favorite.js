45bb847051c6269165091d391b34f6956d64761a
Alberto Zavala <jzavala@efim.com>
2021-11-08 10:56:22 -0700

    Agregando lógica del botón de favoritos

--- src/components/Pokemon/Favorite.js
+++ src/components/Pokemon/Favorite.js
@@ -1,19 +1,50 @@
-import React from 'react'
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import React, { useState, useEffect } from 'react'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { addPokemonFavoriteApi, isPokemonFavoriteApi } from '../../api/favorite';
 
 export const Favorite = ({id}) => {
+  const [isFavorite, setIsFavorite] = useState(undefined);
+  const [reloadCheck, setReloadCheck] = useState(false);
+  const Icon = isFavorite ? FontAwesome : FontAwesome5;
+  console.log(isFavorite);
 
-  const addFavorite = () => {
-    console.log('agregar a favoritos: ', id);
+  useEffect(() => {
+    ( async() => {
+      try{
+        const response = await isPokemonFavoriteApi(id);
+        setIsFavorite(response);
+      }catch(error){
+        setIsFavorite(false);
+      }
+    })();  
+  }, [id, reloadCheck]);
+
+  const onReloadCheckFavorite = () => {
+    setReloadCheck(prev => !prev);
   }
 
-  return (
-    <Icon 
-      name="heart" 
-      color="#fff" 
-      size={20} 
-      onPress={addFavorite} 
-      style={{marginRight: 20}} />
+  const addFavorite = async() => {
+    await addPokemonFavoriteApi(id)    
+    try{
+      onReloadCheckFavorite();
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  const removeFavorite = () => {
+    console.log('Eliminar de Favoritos');
+  }
+
+  return (    
+      <Icon 
+        name="heart" 
+        color="#fff" 
+        size={20} 
+        onPress={ isFavorite ? removeFavorite : addFavorite } 
+        style={{marginRight: 20}} 
+      />    
   )
 }
 
--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, Button } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { getPokemonsFavoriteApi  } from '../api/favorite'
 
-export const Favorite = () => {
+export const Favorite = () => {  
+  
+  const checkFavorites = async () => {
+    const response = await getPokemonsFavoriteApi();
+    console.log(response);
+  }
+
   return (
     <SafeAreaView>
       <Text>Favoritos</Text>
+      <Button title="Obtener Favoritos" onPress={checkFavorites}></Button>
     </SafeAreaView>
   )
 }
